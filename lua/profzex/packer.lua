7f64cc1e7f30680bc7eba61b413ab2de2e84c9ab
ZafeerMahmood <zafeer7460@gmail.com>
2024-04-03 00:37:31 +0500

    added packer.nvim

---
+++ lua/profzex/packer.lua
@@ -0,0 +1,11 @@
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Simple plugins can be specified as strings
+  use 'rstacruz/vim-closer'
+end)
+
