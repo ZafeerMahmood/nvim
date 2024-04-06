2a23f1ab748d8e62fd6ac5f1cd14e2bd433cc95b
ZafeerMahmood <zafeer7460@gmail.com>
2024-04-06 22:01:42 +0500

    added lsp-zero

--- lua/profzex/packer.lua
+++ lua/profzex/packer.lua
@@ -1,16 +1,36 @@
-
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'rstacruz/vim-closer'
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    require = {{'nvim-lua/plenary.nvim'}}
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.6',
+    requires = {'nvim-lua/plenary.nvim'}
   }
-  use('nvim-treesitter/nvim-treesitter', {run=':TSUpdate'})
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
   use 'mbbill/undotree'
-  use "nvim-lua/plenary.nvim"
-  use "tpope/vim-fugitive"  
+  use 'nvim-lua/plenary.nvim'
+  use 'tpope/vim-fugitive'  
 
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      'neovim/nvim-lspconfig',
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      -- Autocompletion
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
+      -- Snippets
+      --'L3MON4D3/LuaSnip',
+      'rafamadriz/friendly-snippets',
+    }
+  }
 end)
+
